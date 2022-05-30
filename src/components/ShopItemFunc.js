12d6b8c2246a9e3fd67338829eb3204e0f27196b
Nikolay Ganibaev <nigani@internet.ru>
2022-05-30 18:46:14 +0300

    Update

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,19 +1,21 @@
 function ShopItemFunc(props) {
+  const {item} = props;
+
   return (
-      <div className="main-content">
-        <h2>{props.item.brand}</h2>
-        <h1>{props.item.title}</h1>
-        <h3>{props.item.description}</h3>
-        <div className="description">{props.item.descriptionFull}</div>
-        <div className="highlight-window mobile">
-          <div className="highlight-overlay"></div>
-        </div>
-        <div className="divider"></div>
-        <div className="purchase-info">
-          <div className="price">{props.item.currency}{props.item.price.toFixed(2)}</div>
-          <button>Добавить в корзину</button>
-        </div>
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
       </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
   );
 }
 
